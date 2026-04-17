c9ac4c0a615c14a1a3c58c583d13ddbfd3207397
Nhựt Minh <minhbui88888888@gmail.com>
2026-04-17 17:08:03 +0700

    Update: Version For Tools

--- tool_image/config.py
+++ tool_image/config.py
@@ -2,4 +2,4 @@ APP_DISPLAY_NAME = "POD Image"
 APP_EXECUTABLE_NAME = "POD_Generate_IMG.exe"
 
 CURRENT_VERSION = "1.0.0"
-UPDATE_JSON_URL = "https://raw.githubusercontent.com/minhbui2003/POD_Image/main/version.json"
+UPDATE_JSON_URL = "https://raw.githubusercontent.com/minhbui2003/POD_AI_Generate/main/version.json"
